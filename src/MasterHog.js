7f2e118000517e1edd1b646aae039e97e7c33fd1
V10LET <violet.wmoon@gmail.com>
2018-10-31 13:12:07 -0500

    destructured props and hogs

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,52 +1,47 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      eyeColor: "blue",
+    constructor() {
+        super()
+        this.state = {
+            eyeColor: "blue"
+        }
     }
-  }
+
+    changeEyeColor = (e) => {
+        this.setState({eyeColor: e.target.value})
+    }
 
 
-  changeEyeColor = (e) => {
-    this.setState({
-      eyeColor: e.target.value
-    })
-  }
 
+    render() {
+        const babyHog = offspring.map(hog => <BabyHog key={hog.id} hog={hog} eyeColor={this.state.eyeColor} />)
+        return (
+            <div>
+                <input type="radio" name="eyeColor" value="blue" onChange={this.changeEyeColor}/>
+                Blue<br></br>
+                <input type="radio" name="eyeColor" value="sun" onChange={this.changeEyeColor}/>
+                Sun<br></br>
+                <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor}/>
+                Glowing<br></br>
 
-  render() {
-    return (
-      <div>
+                <h2>Name: Master Blaster</h2>
+                <h3>Weight: 2.54 Tons</h3>
+                <h3>Eye Color: {this.state.eyeColor}</h3>
 
-        <input type="radio" name="eyeColor" value="blue" onChange={this.changeEyeColor} />
-        Blue<br></br>
-        <input type="radio" name="eyeColor" value="sun" onChange={this.changeEyeColor} />
-        Sun<br></br>
-        <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
-        Glowing<br></br>
-      
-        <h2>Name: Master Blaster</h2>
-        <h3>Weight: 2.54 Tons</h3>
-        <h3>Eye Color: {this.state.eyeColor}</h3>
-        
-        <div id="masters-domicile">
-          <img id="master-blaster" src={Master} alt="" />
-        </div>
-        
-        <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
-        </ul>
+                <div id="masters-domicile">
+                    <img id="master-blaster" src={Master} alt=""/>
+                </div>
 
-      </div>
-    )
-  }
+                <ul className="hoglist">
+                    { babyHog }
+                </ul>
+            </div>
+        )
+    }
 
 }
